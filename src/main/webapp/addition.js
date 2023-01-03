2da0f7372632e94fd30002d9a7514d7f1886fa57
rajanirajanish <rajanirajanish36@gmail.com>
2023-01-03 14:48:12 +0530

    hi

---
+++ src/main/webapp/addition.js
@@ -0,0 +1,7 @@
+function add() {
+var a = document.getElementById("firstno").value
+var b = document.getElementById("secondno").value
+var result = parseInt (a) + parseInt (b)
+document.getElementById("result").value=result
+}
+
